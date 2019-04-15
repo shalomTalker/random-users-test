ffc3c1ec8ca09f95631980683b2875e57a1c13ad
SHTALKER <38102304+shalomTalker@users.noreply.github.com>
2019-04-15 23:07:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,127 @@
-const container = $('#users');
-let list;
+const container = $('#users');// browser tag container
+let localUsers = JSON.parse(localStorage.getItem('localUsers')) || []
 
-function initUsers(list) {
+const findUserIndex = function (localUsers,uuid) {
+    for (let i = 0; i < localUsers.length; i++) {
+        if (localUsers[i].uuid === uuid) {
+            return i;
+        }
+    }
+}
+const editUser = function (user, card) {
+    localUsers = JSON.parse(localStorage.getItem('localUsers'))
+    card.children('.name').html(`
+    <label for="name">Name:</label>
+    <input type="text" name="name" value="${user.name}">`)
 
-    container.empty();
-    
-    list.map(function (user, index) {
-        let card = $('<div>', { class: 'user-card', 'data-id': index })
+    card.children('.email').html(`
+    <label for="email">Email:</label>
+    <input type="text" name="email" value="${user.email}">`)
+
+    card.children('.btns').html('<button class="saveBtn">save</button>')
+
+
+    $('.saveBtn').click(function (e) {
+        let newName = $('[name=name]').val()
+        let newEmail = $('[name=email]').val()
+        let index = findUserIndex(localUsers, user.uuid);
+
+        user.name = newName
+        user.email = newEmail
+
+        
+        localUsers.splice(index, 1, user)
+        localStorage.setItem("localUsers", JSON.stringify(localUsers));
+        renderUsers(localUsers)
+    })
+
+}
+const deleteUser = function (uuid) {
+
+    localUsers = JSON.parse(localStorage.getItem('localUsers'))
+    let index = findUserIndex(localUsers, uuid);
+
+    $(`[data-id=${uuid}]`).remove()
+    localUsers.splice(index, 1)
+    localStorage.setItem('localUsers', JSON.stringify(localUsers))
+}
+
+const fetchUsers = function (url) {
+
+    let usersList = [];
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url,
+            method: 'GET',
+            success: function (data) {
+                for (let i = 0; i < data.results.length; i++) {
+                    const { results } = data
+                    let uuid = results[i].login.uuid
+                    let name = results[i].name.first
+                    let email = results[i].email
+                    let img = results[i].picture.medium
+                    let gender = results[i].gender
+                    let age = results[i].registered.age
+
+                    const userObject = {
+                        uuid: uuid,
+                        name: name,
+                        email: email,
+                        img: img,
+                        gender: gender,
+                        age: age
+                    }
+                    usersList.push(userObject);
+                }
+
+                localStorage.setItem("localUsers", JSON.stringify(usersList))
+                resolve(usersList)
+            },
+            error: function (error) {
+                reject(error)
+            }
+        })
+    })
+
+}
+const renderUsers = function (list) {
+    container.empty()
+    for (let i = 0; i < list.length; i++) {
+        const { name, email, img, gender, age, uuid } = list[i]
+
+        let card = $('<div>', { class: name, 'data-id': uuid })
+
+        let buttonsDiv = $('<div>', { class: 'btns' })
 
         $('<img>', {
-            class: 'image',
-            src: user.image
+            src: img,
+            alt: name
+        }).appendTo(card)
+
+        $('<h3>', {
+            text: 'Name : ' + name,
+            class: "name"
         }).appendTo(card)
 
         $('<span>', {
-            class: 'gender',
-            text: `Gender: ${user.gender}`
+            text: 'Gender : ' + gender
         }).appendTo(card)
 
         $('<span>', {
-            class: 'name',
-            text: `Name: ${user.name}`
+            text: 'Age : ' + age
         }).appendTo(card)
 
         $('<span>', {
-            class: 'age',
-            text: `Age: ${user.age}`
+            text: 'Email : ' + email,
+            class: "email"
         }).appendTo(card)
 
-        $('<span>', {
-            class: 'email',
-            text: `Email: ${user.email}`
-        }).appendTo(card)
-
-        let buttonsDiv = $('<div>', {
-            class: 'btns'
-        })
 
         $('<button>', {
             class: "delete-btn",
             text: "Del",
             click: function () {
-                deleteUser(user, card)
+                deleteUser(uuid)
             }
         }).appendTo(buttonsDiv)
 
@@ -49,93 +129,22 @@ function initUsers(list) {
             class: "edit-btn",
             text: "Edit",
             click: function () {
-                editUser(user, card)
+                editUser(list[i], card)
             }
         }).appendTo(buttonsDiv)
 
         buttonsDiv.appendTo(card)
+
         card.appendTo(container)
 
-    })
+    }
 }
-let storage = JSON.parse(localStorage.getItem("users"));
-
-if (storage && storage.length !== 0) {
-    list = JSON.parse(localStorage.getItem("users"))
-    initUsers(list)
+if (localUsers && localUsers.length !== 0) {
+    renderUsers(localUsers)
 } else {
-
-    list = []
-    $.ajax({
-        url: 'https://randomuser.me/api/?results=10',
-        dataType: 'json',
-        success: function (data) {
-            data.results.map(function (user) {
-                let uuid = user.login.uuid
-                let image = user.picture.large
-                let gender = user.gender
-                let name = user.name.first
-                let age = user.dob.age
-                let email = user.email
-                let object = { uuid, image, gender, name, age, email }
-                list.push(object)
-            })
-            initUsers(list)
-            localStorage.setItem("users", JSON.stringify(list));
-        }
-    });
-
-}
-$('#generateBtn').click(function (e) {
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function (data) {
-            console.log(data)
-            let uuid = data.results[0].login.uuid
-            let image = data.results[0].picture.large
-            let gender = data.results[0].gender
-            let name = data.results[0].name.first
-            let age = data.results[0].dob.age
-            let email = data.results[0].email
-
-            let object = { uuid, image, gender, name, age, email }
-            list.push(object)
-            initUsers(list)
-            localStorage.setItem("users", JSON.stringify(list));
-        }
-    });
-})
-
-
-
-function deleteUser(user, card) {
-    list.splice(card['data-id'],1)
-    card.remove()
-    localStorage.setItem("users", JSON.stringify(list));
-}
-function editUser(user, card) {
-    
-    card.children('.name').html(`
-    <label for="name">Name:</label>
-    <input type="text" name="name" value="${user.name}">`)
-    
-    card.children('.email').html(`
-    <label for="email">Email:</label>
-    <input type="text" name="email" value="${user.email}">`)
-    
-    card.children('.btns').html('<button class="saveBtn">save</button>')
-
-
-    $('.saveBtn').click(function (e) {
-        let newName = $('[name=name]').val()
-        let newEmail = $('[name=email]').val()
-
-        user.name = newName
-        user.email = newEmail
-
-        initUsers(list)
-        localStorage.setItem("users", JSON.stringify(list));
-    })
+    fetchUsers('https://randomuser.me/api/?results=10')
+        .then(localUsers => {
+            renderUsers(localUsers);
+        })
 
 }
